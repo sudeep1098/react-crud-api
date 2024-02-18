0aa407151ab7ca18263a617fc783ecce9aad1d9a
Sudeep <sudeep1098@gmail.com>
2024-02-18 14:40:07 +0530

    added redux.jsx

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Create from "./components/Create";
 import Read from "./components/Read";
 import Edit from "./components/Edit";
+import Redux from "./components/Redux";
 function App() {
   return (
     <div className="container">
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Read />}></Route>
         <Route path="/create" element={<Create />}></Route>
         <Route path="/edit" element={<Edit />}></Route>
+        <Route path="/count" element={<Redux />}></Route>
       </Routes>
     </div>
   );
